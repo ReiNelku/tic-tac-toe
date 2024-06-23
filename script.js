26667fcde081a7546490e74e7be56c5b430c7562
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2024-06-23 10:59:46 +0200

    feat: Create Player module

--- script.js
+++ script.js
@@ -6,23 +6,39 @@ function GameBoard() {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push("Empty");
+      board[i].push(Player());
     }
   }
 
   const getBoard = () => board;
 
   const selectSquare = (row, column, player) => {
-    if (board[row][column] !== "Empty") {
+    if (board[row][column].getSymbol() !== "Empty") {
       return;
     }
 
-    board[row][column] = player;
+    board[row][column].addSymbol(player);
   };
 
   const printBoard = () => {
-    console.log(board);
+    const boardWithSymbolValues = board.map((row) =>
+      row.map((square) => square.getSymbol())
+    );
+
+    console.log(boardWithSymbolValues);
   };
 
   return { getBoard, selectSquare, printBoard };
+}
+
+function Player() {
+  let symbol = "Empty";
+
+  const addSymbol = (player) => {
+    symbol = player;
+  };
+
+  const getSymbol = () => symbol;
+
+  return { addSymbol, getSymbol };
 }
